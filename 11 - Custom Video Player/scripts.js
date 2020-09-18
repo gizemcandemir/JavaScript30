ca7998b24edb20833bf5eab04235ec151f4c2828
Gizem Candemir <gizemc@gmail.com>
2020-09-18 21:25:30 +0200

    Add initial handleUpdateRange function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -5,7 +5,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const range = player.querySelectorAll('player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 // Build out functions
 
@@ -23,6 +23,10 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  console.log(this.value);
+}
+
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -31,3 +35,5 @@ video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach( button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
