8821ce088c8f778ac7919b1373ce06ee3ac38c2d
Gizem Candemir <gizemc@gmail.com>
2020-09-18 21:13:47 +0200

    Add functionality to skip

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -18,9 +18,16 @@ function updateButton() {
   toggle.textContent = this.paused ? '►' : '❚ ❚';
 }
 
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
 // Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach( button => button.addEventListener('click', skip));
